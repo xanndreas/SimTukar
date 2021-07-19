bfdf7614ba87675b43a7017ef459479f8950f93f
I am andreas <36619031+xanndreas@users.noreply.github.com>
2021-07-18 21:49:28 -0700

    revision 2

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -11,11 +11,14 @@ class UsersTableSeeder extends Seeder
     {
         $users = [
             [
-                'id'             => 1,
-                'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => bcrypt('password'),
-                'remember_token' => null,
+                'id'                 => 1,
+                'name'               => 'Admin',
+                'email'              => 'admin@admin.com',
+                'password'           => bcrypt('password'),
+                'remember_token'     => null,
+                'verified'           => 1,
+                'verified_at'        => '2021-07-08 05:03:29',
+                'verification_token' => '',
             ],
         ];
 
